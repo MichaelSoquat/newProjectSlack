cf35114b5e00da1356f508fb0211849931eb276f
MichaelSoquat <michael.soquat@gmx.de>
2022-03-20 15:27:23 +0100

    second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'projectslack-db44b',
+    appId: '1:243343576662:web:6213ec7326177265a5b5f7',
+    storageBucket: 'projectslack-db44b.appspot.com',
+    apiKey: 'AIzaSyAvFQsL4Gi7WvNid93wnOSmQv4s2tclB2k',
+    authDomain: 'projectslack-db44b.firebaseapp.com',
+    messagingSenderId: '243343576662',
+  },
   production: true
 };

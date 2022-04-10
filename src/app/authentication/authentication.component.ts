b1569c22dbc9fa9a742db0cacdb63c8647a14b63
SebSeitz <sebastian_seitz13@web.de>
2022-04-10 17:00:44 +0200

    go tosignin section after signup

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { MatTabGroup } from '@angular/material/tabs';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { EmailAuthCredential } from 'firebase/auth';
 import { Channel } from 'src/models/channel.class';
 import { Chatroom } from 'src/models/chatroom.class';
--- src/app/main-chat/main-chat.component.ts
+++ src/app/main-chat/main-chat.component.ts
@@ -28,13 +28,13 @@ export class MainChatComponent implements OnInit {
 
 
   getMessageImageSource() {
-      this.userName = this.message.from;
-      for (let i = 0; i < this.backend.data.users.length; i++) {
-        const user = this.backend.data.users[i];
-        if (user.name == this.userName) {
-          return user.image;
-        }
-      }
+      // this.userName = this.message.from;
+      // for (let i = 0; i < this.backend.data.users.length; i++) {
+      //   const user = this.backend.data.users[i];
+      //   if (user.name == this.userName) {
+      //     return user.image;
+      //   }
+      // }
     return "https://i.pravatar.cc/24?img=1";
   }
 

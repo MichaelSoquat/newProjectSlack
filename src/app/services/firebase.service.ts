afed0503cf9db86b6d8db99aa7c2debf01db1d4d
SebSeitz <sebastian_seitz13@web.de>
2022-04-14 15:07:21 +0200

    added new error messages for login

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { TabIndex } from '../interface/TabIndex.interface';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogExampleComponent } from '../dialog-example/dialog-example.component';
+import { AuthenticationComponent } from '../authentication/authentication.component';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ import { DialogExampleComponent } from '../dialog-example/dialog-example.compone
 export class FirebaseService {
   isloggedIn = false
   constructor(public firebaseAuth: AngularFireAuth,
-    public backend: BackendService, private router: Router,  public dialog: MatDialog,) {
+    public backend: BackendService, private router: Router, public dialog: MatDialog,) {
   }
 
   async signin(email: string, password: string) {
@@ -23,16 +24,13 @@ export class FirebaseService {
         localStorage.setItem('user', JSON.stringify(response.user))
         console.log('user is', response.user);
         this.router.navigate(['/main']);
-      }).catch(function(error) {// Handle Errors here.
+      }).catch(function (error) {// Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        if (errorCode === 'auth/wrong-password') {
-          alert('Wrong password.');
-        } else {
-          alert(errorMessage);
-        }
+        document.querySelector('.Loginerror').innerHTML = errorMessage;
         console.log(error);
-      });}
+      });
+  }
 
   // sign up with email and password
 
@@ -40,15 +38,15 @@ export class FirebaseService {
     await this.firebaseAuth.createUserWithEmailAndPassword(email, password)
       .then(response => {
         this.isloggedIn = true;
-        localStorage.setItem('user', JSON.stringify(response.user))
-        alert('you are now signed up');
-        tabIndex.index = 0;
-      }).catch(function(error) {// Handle Errors here.
-        var errorMessage = error.message;
-        alert(errorMessage);
-        console.log(error);
+        localStorage.setItem('user', JSON.stringify(response.user));
         this.dialog.open(DialogExampleComponent);
-  });}
+        document.querySelector('.error').innerHTML = '';
+        tabIndex.index = 0;
+      }).catch(function (error) {// Handle Errors here.
+        var errorMessage = error.message;
+        document.querySelector('.error').innerHTML = errorMessage;
+      });
+  }
 
   // logout
 
